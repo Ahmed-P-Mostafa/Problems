c4bfbbd334fb3fc05233cc68c0491734206e5282
ahmed <ahmed.mostfa.ghorab@gmail.com>
2021-08-18 18:49:16 +0200

    7

--- FinishedProblems.cpp
+++ FinishedProblems.cpp
@@ -10,7 +10,26 @@
    * my submission link
    * https://codeforces.com/contest/112/submission/126148531*/
 
+void dislikeOfThree(){
 
+    int t ;
+    std::cin >> t;
+
+    for (int i = 1; i <=t; ++i) {
+        int k;
+        std::cin >> k;
+        std::string  kk = std::to_string(k);
+        char last = kk.at(kk.length()-1);
+        if (k % 3 == 0|| last=='3'){
+            t++;
+        }else{
+            std::cout << i <<std::endl;
+
+        }
+
+    }
+
+}
 void petyaAndString(){
     /** 6
    * my submission link
