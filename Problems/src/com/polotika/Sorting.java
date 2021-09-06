29f144f79a54c30dbbe8ce778ad75ff17b26dfa5
ahmed <ahmed.mostfa.ghorab@gmail.com>
2021-09-06 23:45:33 +0200

    Selection sort

--- Problems/src/com/polotika/Sorting.java
+++ Problems/src/com/polotika/Sorting.java
@@ -1,6 +1,8 @@
 package com.polotika;
 
 public class Sorting {
+    // int[] array = new int[]{3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
+
     static void insertionIncreasingSort(int[] arr){
         // int[] array = new int[]{3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
         int j ,key;
@@ -23,10 +25,6 @@ public class Sorting {
     }
 
     static void insertionDecreasingSort(int[] arr){
-        // int[] array = new int[]{3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
-
-        //   3,44,38,5,47,15,36,26,27,2,46,4,19,50,48
-
         int j,key;
 
         for (int i = 1; i< arr.length;i++){
@@ -40,7 +38,7 @@ public class Sorting {
         }
 
         for (int i:arr) {
-            System.out.print(i+" ");
+            System.out.print(i +" ");
 
         }
         System.out.println();
@@ -65,5 +63,29 @@ public class Sorting {
             System.out.print(i+" ");
         }
         System.out.println();
+    }
+
+    static void selectionDecreasingSort(int[] arr){
+        int h , j , t;
+
+        for (int i = 0; i <arr.length;i++){
+            h= arr[i];
+            t = i;
+            for (j = i+1;j< arr.length;j++){
+                if (arr[j]>h){
+                    h = arr[j];
+                    t = j;
+                }
+            }
+
+            arr[t] = arr[i];
+            arr[i] = h;
+        }
+
+        for (int i : arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
     }
 }
